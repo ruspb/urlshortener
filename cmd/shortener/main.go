b259ad3d94b85f2fff777881031f9f86d6c1e0cf
Kutyrev.AA <!ZSry9KRiUDkW>
2022-07-11 23:26:00 +0300

    реализация

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,11 +8,32 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.POST("/", url.CreateUrl)
-	router.GET("/:id", url.ReadUrl)
+	router.POST("/", func(c *gin.Context) {
+		longUrl, err := c.GetRawData()
+		if err != nil {
+			c.String(http.StatusBadRequest, "Не передан Url")
+		}
+
+		result, err := url.Create(string(longUrl))
+		if err != nil {
+			c.String(http.StatusBadRequest, "Ошибка сохранения Url")
+		}
+
+		c.String(http.StatusCreated, "http://localhost:8080/"+result)
+	})
+	router.GET("/:id", func(c *gin.Context) {
+		result, err := url.Read(c.Param("id"))
+		if err != nil {
+			c.String(http.StatusBadRequest, "Ошибка чтения Url")
+		}
+
+		c.Header("Location", result)
+		c.String(http.StatusTemporaryRedirect, "")
+		//c.String(http.StatusOK, result)
+	})
 
 	router.NoRoute(func(c *gin.Context) {
-		c.String(http.StatusBadRequest, "Bad Request")
+		c.String(http.StatusBadRequest, "Несуществуюший Route")
 	})
 
 	router.Run(":8080")
--- internal/app/url.go
+++ internal/app/url.go
@@ -1,15 +1,46 @@
 package url
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"errors"
+	"math/rand"
 )
 
-func CreateUrl(c *gin.Context) {
-	c.String(http.StatusOK, "POST /")
+var urls = map[string]string{}
+
+func Create(longUrl string) (string, error) {
+	shortUrl, _ := find(longUrl)
+	if len([]rune(shortUrl)) > 0 {
+		return shortUrl, nil
+	}
+
+	shortUrl = randomString(4)
+	urls[shortUrl] = longUrl
+	return shortUrl, nil
 }
 
-func ReadUrl(c *gin.Context) {
-	short := c.Param("id")
-	c.String(http.StatusOK, "GET "+short)
+func Read(shortUrl string) (string, error) {
+	longUrl, exists := urls[shortUrl]
+	if !exists {
+		return "", errors.New("url не найден")
+	}
+
+	return longUrl, nil
+}
+
+func randomString(n int) string {
+	var chars = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321")
+	str := make([]rune, n)
+	for i := range str {
+		str[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(str)
+}
+
+func find(longUrl string) (string, string) {
+	for key, value := range urls {
+		if value == longUrl {
+			return key, value
+		}
+	}
+	return "", ""
 }
